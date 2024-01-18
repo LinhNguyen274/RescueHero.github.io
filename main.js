765d3ab48dac2381ef2a2d430cd8bdc8cc69869c
Ivan_Nguyen_CEOnline <120379173+LinhNguyen274@users.noreply.github.com>
2024-01-18 15:14:47 +0700

    rescueHero

--- main.js
+++ main.js
@@ -2,26 +2,34 @@ window.boot = function () {
     var settings = window._CCSettings;
     window._CCSettings = undefined;
     var onProgress = null;
-    
+
     var RESOURCES = cc.AssetManager.BuiltinBundleName.RESOURCES;
     var INTERNAL = cc.AssetManager.BuiltinBundleName.INTERNAL;
     var MAIN = cc.AssetManager.BuiltinBundleName.MAIN;
-    function setLoadingDisplay () {
+    function setLoadingDisplay() {
         // Loading splash scene
         var splash = document.getElementById('splash');
-        var progressBar = splash.querySelector('.progress-bar span');
+        var progressBar2 = splash.querySelector('.progress-bar span');
         onProgress = function (finish, total) {
             var percent = 100 * finish / total;
-            if (progressBar) {
-                progressBar.style.width = percent.toFixed(2) + '%';
+            // if (progressBar) {
+            //     progressBar.style.width = percent.toFixed(2) + '%';
+            // }
+            // progressBar(percent)
+            if (window.progressBar) {
+                progressBar(percent)
+            }
+            window.getLoadingPerc = function () {
+                return percent
             }
         };
         splash.style.display = 'block';
-        progressBar.style.width = '0%';
+        progressBar2.style.width = '0%';
 
         cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
             splash.style.display = 'none';
         });
+
     }
 
     var onStart = function () {
@@ -63,7 +71,7 @@ window.boot = function () {
         var bundle = cc.assetManager.bundles.find(function (b) {
             return b.getSceneInfo(launchScene);
         });
-        
+
         bundle.loadScene(launchScene, null, onProgress,
             function (err, scene) {
                 if (!err) {
@@ -77,6 +85,11 @@ window.boot = function () {
                             div.style.backgroundImage = '';
                         }
                         console.log('Success to load scene: ' + launchScene);
+                        cc.sys.localStorage.setItem("firstTime", JSON.stringify(true));
+                        refreshStickyBannerAd();
+                        StickyBannerInstance = window?.GlanceGamingAdInterface?.showStickyBannerAd(StickyObj, bannerCallbacks);
+                        replayInstance = window.GlanceGamingAdInterface.loadRewardedAd(replayObj, rewardedCallbacks);
+                        rewardInstance = window.GlanceGamingAdInterface.loadRewardedAd(rewardObj, rewardedCallbacks);
                     }
                 }
             }
@@ -93,17 +106,17 @@ window.boot = function () {
         collisionMatrix: settings.collisionMatrix,
     };
 
-    cc.assetManager.init({ 
+    cc.assetManager.init({
         bundleVers: settings.bundleVers,
         remoteBundles: settings.remoteBundles,
         server: settings.server
     });
-    
+
     var bundleRoot = [INTERNAL];
     settings.hasResourcesBundle && bundleRoot.push(RESOURCES);
 
     var count = 0;
-    function cb (err) {
+    function cb(err) {
         if (err) return console.error(err.message, err.stack);
         count++;
         if (count === bundleRoot.length + 1) {
@@ -113,7 +126,7 @@ window.boot = function () {
         }
     }
 
-    cc.assetManager.loadScript(settings.jsList.map(function (x) { return 'src/' + x;}), cb);
+    cc.assetManager.loadScript(settings.jsList.map(function (x) { return 'src/' + x; }), cb);
 
     for (var i = 0; i < bundleRoot.length; i++) {
         cc.assetManager.loadBundle(bundleRoot[i], cb);
